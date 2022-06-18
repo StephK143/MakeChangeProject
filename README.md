302ae463264bd78d65f928d3f306695c64b56ee7
Steph Karlsen <becomingsteph@gmail.com>
2022-06-17 19:17:39 -0600

    MakeChange initial commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # MakeChangeProject
 
-## This is a cash register
+
+## Cash register
+This cash register returns the change due to the customer in denominations. The value of the balance is broken down into coins and bills so that the user can just make change without doing any math.
 
 ##Technologies used
 
+
 ## What I learned
+For rounding: I didn't want to use the Decimal.format because this makes your double into a string. Then you can't make calculations so I used (int)(Math.round(num1 * 100)). 
+The first time I wrote this it was considerably longer. I am learning to shorten the code for easier readability. 
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -1,10 +1,140 @@
 package com.skilldistillery.makechange;
 
+//import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner kb = new Scanner(System.in);
+		double itemPrice = 0.00;
+		double cashTender = 0.00;
+		
+		System.out.println("What is the price of your item?");
+		itemPrice = kb.nextDouble();
 
-	}
+		System.out.println("How much money did the customer give you?");
+		cashTender = kb.nextDouble();
+		
+		custBalance(itemPrice, cashTender);
+	
+		kb.close();
+		
+	}	
+		public static double custBalance(double price, double tender) {
+		
+			double custBalance = (tender - price);
+			custBalance = (double)Math.round(custBalance * 100d) /100d;
+		//	System.out.println(custBalance);
+			
+			
+			
+			
+			if (custBalance == 0) {
+				printZeroBalance();
+				
+					
+			}	else if (custBalance > 0){
+					
+					System.out.println("The customer's change is " + "$" + custBalance);
+					exactChangeDue(custBalance);
+					
+			}	else {
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+					
+					System.out.println("************************************");
+					System.out.println("******* The customer did not *******");
+					System.out.println("****  give you enough money ********");
+					System.out.println("*********  Request additional  *****");
+					System.out.println("*************** $"+ (custBalance * -1) + " **************");
+					System.out.println("************************************");
+					  
+					 
+		}
+			return custBalance;
+			}
+			
+		public static String printZeroBalance() {
+			
+			
+			String zeroBalance = "**********************************************\n"
+								+ "** The customer has given you exact change **\n"
+								+ "************* Balance is zero ***************\n"
+								+ "*********************************************\n";
+			
+			System.out.println(zeroBalance);
+			
+						
+						return zeroBalance;
+				}
+			
+		
+		public static double exactChangeDue(double custBalance) {
+			
+			double count = 0.00;
+		//	custBalance = (double)Math.round(custBalance * 100d) /100d;
+			
+			
+		while ( count >= 0) {
+			
+			if (custBalance >= 10) { 
+				int tenDollarBill = (int)(custBalance / 10);
+				System.out.println(tenDollarBill + " ten dollar bill." );
+				count = custBalance = custBalance % 10;
+				custBalance = (double)Math.round(custBalance * 100d) /100d;
+			//	System.out.println(custBalance);
+				
+			} else if (custBalance >= 5) {
+					int fiveDollarBill = (int) (custBalance / 5);
+					System.out.println(fiveDollarBill + " five dollar bill." );
+					count = custBalance = custBalance % 5;
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+				//	System.out.println(custBalance);
+			
+			} else if (custBalance >= 1) {
+					int oneDollarBill = (int) (custBalance / 1);
+					System.out.println(oneDollarBill + " one dollar bill." );
+					count = custBalance = custBalance % 1;
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+			//		System.out.println(custBalance);
+					
+			}else if (custBalance >= 0.25)	{
+					int quarterCoin = (int) (custBalance / .25);
+					System.out.println(quarterCoin + " quarter." );
+					count = custBalance = custBalance % 0.25;
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+			//		System.out.println(custBalance);
+					
+			} else if (custBalance >= 0.10) {
+					int dimeCoin = (int) (custBalance / .10);
+					System.out.println((dimeCoin) + " dime." );
+					count = custBalance = custBalance % 0.10;
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+			//		System.out.println(custBalance);
+					
+			} else if (custBalance >= 0.05)	{
+					int nickelCoin = (int) (custBalance / .05);
+					System.out.println((nickelCoin) + " nickel." );
+					count = custBalance = custBalance % 0.05;
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+			//		System.out.println(custBalance);
+					
+			} else if (custBalance >= .01) { 
+					int pennyCoin = (int) (custBalance / .01 + .007);
+					System.out.println((pennyCoin) + " penny." );
+				//	count = custBalance = custBalance % 0.01;
+					custBalance = (double)Math.round(custBalance * 100d) /100d;
+			//		System.out.println(custBalance);
+					
+					break;
+					
+					
+				}
+			}
+			return count;
+			}
 
 }
+
+
